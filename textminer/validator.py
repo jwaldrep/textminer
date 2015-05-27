435cb6a43f4b489a7c2cfe92415e80d6b58a6eb0
John Waldrep <johnwaldrep@gmail.com>
2015-05-27 14:29:09 -0400

    Add money() with passing test; 7/70 passing

--- textminer/validator.py
+++ textminer/validator.py
@@ -27,8 +27,10 @@ def phone_number(text):
     return re.match(r'\(?\d{3}\)?[ \-\.]?\d{3}[ \-\.]?\d{4}', text)
 
 def money(text):
-    return re.match(r"""^\$(\d{1,3})  # $ and at least 1 digit, 3 max
-
-                           (,\d{3})*   # commas require 3 trailing digits (opt)
-                           (.\d{2})?$  # dec require 2 trailing digits (opt)
+    return re.match(r"""^\$(((\d{1,3})  # $ and at least 1 digit, 3 max
+                           (,\d{3})+)   # commas require 3 trailing digits (opt)
+                           |            # unless there are no commas, in
+                           (\d+))       # which case we just want 1+ digits
+                           (\.\d{2})?$  # dec require 2 trailing digits (opt)
                            """, text, re.VERBOSE)
+    # return re.match(r'^\$(((\d{1,3})(,\d{3})+)|(\d+))(\.\d{2})?$', text)
