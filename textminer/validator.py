5fa342c4f9860fdbc792076992f424e9397ad138
John Waldrep <johnwaldrep@gmail.com>
2015-05-27 14:02:21 -0400

    More work on money

--- textminer/validator.py
+++ textminer/validator.py
@@ -27,4 +27,8 @@ def phone_number(text):
     return re.match(r'\(?\d{3}\)?[ \-\.]?\d{3}[ \-\.]?\d{4}', text)
 
 def money(text):
-    return re.match(r'^\$(\d{1,3})+', text)
+    return re.match(r"""^\$(\d{1,3})  # $ and at least 1 digit, 3 max
+
+                           (,\d{3})*   # commas require 3 trailing digits (opt)
+                           (.\d{2})?$  # dec require 2 trailing digits (opt)
+                           """, text, re.VERBOSE)
